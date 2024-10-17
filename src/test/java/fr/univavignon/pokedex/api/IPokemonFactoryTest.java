0e038ec704ad7230e823424dd32141395837fe32
dylanmac <dylan.mastrolia@alumni.univ-avignon.fr>
2024-10-18 01:27:30 +0200

    Ajoute un test de squelette pour IPokemonFactory et modifie le job de test CircleCI
    Un fichier de test initial pour IPokemonFactory a été ajouté. La commande de test CircleCI est modifiée pour exécuter "mvn test".

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -0,0 +1,10 @@
+package fr.univavignon.pokedex.api;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class IPokemonFactoryTest {
+    private IPokemonFactory factory;
+}
