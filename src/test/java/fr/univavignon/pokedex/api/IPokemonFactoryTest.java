890210077d451ca158504a9288dcb2c651b2a2f0
dylanmac <dylan.mastrolia@alumni.univ-avignon.fr>
2024-10-18 01:49:03 +0200

    Ajoute un test pour la méthode createPokemon dans IPokemonFactoryTest
    Initialise un mock pour IPokemonFactory et vérifie que le Pokémon est correctement créé avec les attributs spécifiés.

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -7,4 +7,18 @@ import org.mockito.Mockito;
 
 public class IPokemonFactoryTest {
     private IPokemonFactory factory;
+
+    @Before
+    public void init(){
+        factory = Mockito.mock(IPokemonFactory.class);
+    }
+
+    @Test
+    public void testCreatePokemon() throws Exception{
+        Pokemon pokemon = new Pokemon(1,"Pika",100,120,200,5,40,20,30,43.00);
+        Mockito.when(factory.createPokemon(1,5,40,20,30)).thenReturn(pokemon);
+        Pokemon result = factory.createPokemon(1,5,40,20,30);
+        assertNotNull(result);
+        assertEquals(5, result.getCp());
+    }
 }
