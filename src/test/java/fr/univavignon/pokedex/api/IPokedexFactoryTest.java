fd101ce6248f6bcda5dfbe0b71b3b24112976177
dylanmac <dylan.mastrolia@alumni.univ-avignon.fr>
2024-10-18 02:01:36 +0200

    Ajoute les tests unitaires pour IPokedexFactory
    Création d'une nouvelle classe de tests unitaires IPokedexFactoryTest utilisant Mockito pour valider la méthode createPokedex.

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -0,0 +1,30 @@
+package fr.univavignon.pokedex.api;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class IPokedexFactoryTest {
+    private IPokedexFactory factory;
+    private IPokemonFactory pokemonFactory;
+    private IPokemonMetadataProvider metadataProvider;
+
+    @Before
+    public void init(){
+        factory = Mockito.mock(IPokedexFactory.class);
+        pokemonFactory = Mockito.mock(IPokemonFactory.class);
+        metadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+    }
+
+    @Test
+    public void testCreatePokedex() throws Exception{
+        IPokedex pokedex = Mockito.mock(IPokedex.class);
+        Mockito.when(factory.createPokedex(metadataProvider,pokemonFactory)).thenReturn(pokedex);
+        IPokedex result = factory.createPokedex(metadataProvider,pokemonFactory);
+        assertNotNull(result);
+        Mockito.verify(factory).createPokedex(metadataProvider,pokemonFactory);
+
+
+    }
+}
