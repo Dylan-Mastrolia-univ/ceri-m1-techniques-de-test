7d6ef5d2a98b5070bcf64f1dc8a33028d7783b1e
dylanmac <dylan.mastrolia@alumni.univ-avignon.fr>
2024-11-04 09:47:56 +0100

    Test du badge

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -8,7 +8,7 @@ import org.mockito.Mockito;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-
+import java.util.ArrayList;
 public class IPokedexTest {
     private IPokedex pokedex;
 
