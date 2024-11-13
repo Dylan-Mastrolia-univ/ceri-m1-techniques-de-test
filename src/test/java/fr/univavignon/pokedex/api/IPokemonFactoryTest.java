6ca72ab3d45c07260ac0f1182450f6e11e5153fd
dylanmac <dylan.mastrolia@alumni.univ-avignon.fr>
2024-11-13 12:17:21 +0100

    revoke d'un test

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -1,32 +1,30 @@
 package fr.univavignon.pokedex.api;
-
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-
+import org.mockito.Mockito;
 public class IPokemonFactoryTest {
     private IPokemonFactory factory;
 
     @Before
-    public void init() {
-        factory = new RocketPokemonFactory();
+    public void init(){
+        factory = Mockito.mock(IPokemonFactory.class);
     }
 
     @Test
-    public void testCreatePokemon() throws Exception {
-        Pokemon result = factory.createPokemon(0, 613, 64, 4000, 4);
-
+    public void testCreatePokemon() throws Exception{
+        Pokemon pokemon = new Pokemon(0,"Bulbizarre",126,126,90,613,64,4000,4,0.56);
+        Mockito.when(factory.createPokemon(0,613,64,4000,4)).thenReturn(pokemon);
+        Pokemon result = factory.createPokemon(0,613,64,4000,4);
         assertNotNull(result);
-
-        assertEquals(0, result.getIndex());
-        assertEquals("MISSINGNO", result.getName());
+        assertEquals(0,result.getIndex());
         assertEquals(613, result.getCp());
         assertEquals(64, result.getHp());
-        assertEquals(4000, result.getDust());
-        assertEquals(4, result.getCandy());
+        assertEquals(4000,result.getDust());
+        assertEquals(4,result.getCandy());
+        assertEquals(0.56,result.getIv(),0);
 
 
-        assertEquals(1.0, result.getIv(), 0);
     }
 }
