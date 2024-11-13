99ddb8a88e2eec1c419d86510c996e1702fb184a
dylanmac <dylan.mastrolia@alumni.univ-avignon.fr>
2024-11-13 12:11:42 +0100

    Fix error

--- src/main/java/fr/univavignon/pokedex/api/RocketPokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/RocketPokemonFactory.java
@@ -5,26 +5,23 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-
 public class RocketPokemonFactory implements IPokemonFactory {
-	
+
 	private static Map<Integer, String> index2name;
 	static {
 		Map<Integer, String> aMap = new HashMap<Integer, String>();
-        aMap.put(-1, "Ash's Pikachu");
-        aMap.put(0, "MISSINGNO");
-        aMap.put(1, "Bulbasaur");
-        //TODO : Gotta map them all !
-        index2name = Collections.unmodifiableMap(aMap);
+		aMap.put(-1, "Ash's Pikachu");
+		aMap.put(0, "MISSINGNO");
+		aMap.put(1, "Bulbasaur");
+		index2name = Collections.unmodifiableMap(aMap);
 	}
-	
+
 	private static int generateRandomStat() {
 		int total = 0;
-		for(int i=0; i < 1000000; i++)
-		{
+		for (int i = 0; i < 1000000; i++) {
 			Random rn = new Random();
-		    int r = rn.nextInt(2);
-		    total = total + r;
+			int r = rn.nextInt(2);
+			total = total + r;
 		}
 		return total / 10000;
 	}
@@ -32,7 +29,7 @@ public class RocketPokemonFactory implements IPokemonFactory {
 	@Override
 	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
 		String name;
-		if(!index2name.containsKey(index)) {
+		if (!index2name.containsKey(index)) {
 			name = index2name.get(0);
 		} else {
 			name = index2name.get(index);
@@ -41,7 +38,7 @@ public class RocketPokemonFactory implements IPokemonFactory {
 		int defense;
 		int stamina;
 		double iv;
-		if(index < 0) {
+		if (index < 0) {
 			attack = 1000;
 			defense = 1000;
 			stamina = 1000;
@@ -54,5 +51,4 @@ public class RocketPokemonFactory implements IPokemonFactory {
 		}
 		return new Pokemon(index, name, attack, defense, stamina, cp, hp, dust, candy, iv);
 	}
-
 }
