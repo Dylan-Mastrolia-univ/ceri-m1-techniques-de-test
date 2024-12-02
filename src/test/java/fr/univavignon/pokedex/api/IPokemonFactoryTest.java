cdc57a8fb2c991a9cde9181fc59539d5d066d044
dylanmac <dylan.mastrolia@alumni.univ-avignon.fr>
2024-12-02 03:26:47 +0100

    Ajoute des tests unitaires pour la création de Pokémon avec des index spécifiques.

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -42,4 +42,48 @@ public class IPokemonFactoryTest {
             assertTrue( pokemon.getIv() >= 0 && pokemon.getIv() <= 100);
         }
     }
+
+    @Test
+    public void testMissingNoName() {
+        Pokemon pokemon = factory.createPokemon(0, 0, 0, 0, 0);
+
+        assertNotNull(pokemon);
+        assertEquals("MISSINGNO", pokemon.getName());
+    }
+
+    @Test
+    public void testCreatePokemonIndexNegatif() throws Exception {
+        Pokemon pokemon = factory.createPokemon(-1, 500, 50, 1000, 5);
+
+        assertNotNull(pokemon);
+        assertEquals( -1, pokemon.getIndex());
+        assertEquals( 500, pokemon.getCp());
+        assertEquals(50, pokemon.getHp());
+        assertEquals( 1000, pokemon.getDust());
+        assertEquals( 5, pokemon.getCandy());
+
+        assertEquals(1000, pokemon.getAttack());
+        assertEquals(1000, pokemon.getDefense());
+        assertEquals( 1000, pokemon.getStamina());
+        assertEquals(0, pokemon.getIv(), 0);
+    }
+
+    @Test
+    public void testCreatePokemonIndexInconnu() throws Exception {
+        Pokemon pokemon = factory.createPokemon(999, 300, 30, 100, 1);
+
+        assertNotNull(pokemon);
+        assertEquals( 999, pokemon.getIndex());
+        assertEquals( 300, pokemon.getCp());
+        assertEquals( 30, pokemon.getHp());
+        assertEquals( 100, pokemon.getDust());
+        assertEquals( 1, pokemon.getCandy());
+
+        assertTrue(pokemon.getAttack() >= 126 && pokemon.getAttack() <= 141);
+        assertTrue( pokemon.getDefense() >= 126 && pokemon.getDefense() <= 141);
+        assertTrue(pokemon.getStamina() >= 90 && pokemon.getStamina() <= 105);
+
+        assertTrue( pokemon.getIv() >= 0 && pokemon.getIv() <= 100);
+    }
+
 }
